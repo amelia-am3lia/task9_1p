4adb654a5d948a517c4192016f739dcd4637cdd8
amelia <ameliaholly7@gmail.com>
2026-05-23 21:54:14 +1000

    delete files and add new ones, changing pproach after researching correct methods etc

--- src/main/java/web/service/Assessment.java
+++
@@ -1,9 +0,0 @@
-package web.service;
-import java.time.LocalDate;
-
-public class Assessment{
-	private LocalDate dateDue;
-	public LocalDate DateDue() {
-		return dateDue;
-	}
-}
---
+++ src/test/java/web/service/web/TestTaskFeedbackService.java
@@ -0,0 +1,12 @@
+package web.service;
+import org.junit.*;
+import java.time.LocalDate;
+
+public class TestTaskFeedbackService {
+	@Test 
+	public void testSubmittedBeforeDueSuccess() {
+		LocalDate dueDate; 
+		Localdate dateSubmitted;
+		TaskFeedbackService feedbackChecker = new TaskFeedbackService;
+	}
+}
