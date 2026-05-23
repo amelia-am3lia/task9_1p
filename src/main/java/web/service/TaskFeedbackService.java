68a42bb2d68f5efe724f6bd86bd49042cc9b4284
amelia <ameliaholly7@gmail.com>
2026-05-24 09:00:47 +1000

    Move null check to start of code block to prevent error

--- src/main/java/web/service/TaskFeedbackService.java
+++ src/main/java/web/service/TaskFeedbackService.java
@@ -3,11 +3,13 @@ import java.time.LocalDate;
 
 public class TaskFeedbackService{	
 	public static String Verification(LocalDate DateSubmitted, LocalDate DueDate){
+		if (DateSubmitted == null && LocalDate.now().isAfter(DueDate)) {
+			return "Feedback will not be given on your assessment.";
+		}
 		if(DateSubmitted.isBefore(DueDate) || DateSubmitted.isEqual(DueDate)) {
 			return "Feedback will be given on your assessment.";
 		}
-		else if (DateSubmitted.isAfter(DueDate) || 
-				(DateSubmitted == null && LocalDate.now().isAfter(DueDate))) {
+		else if (DateSubmitted.isAfter(DueDate) {
 			return "Feedback will not be given on your assessment.";
 		}
 		else return "Please contact your tutor.";
