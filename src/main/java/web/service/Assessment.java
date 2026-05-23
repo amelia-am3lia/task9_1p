9f435a00a76a397f4879d55c3fb0c0c390a0ab89
amelia <ameliaholly7@gmail.com>
2026-05-23 19:39:25 +1000

    add date due private member and public return method to assessment class so test will work

--- src/main/java/web/service/Assessment.java
+++ src/main/java/web/service/Assessment.java
@@ -1,5 +1,9 @@
 package web.service;
+import java.time.LocalDate;
 
 public class Assessment{
-	
+	private LocalDate dateDue;
+	public LocalDate DateDue() {
+		return dateDue;
+	}
 }
--- src/test/java/web/service/assessmentDueDateStatusTest.java
+++ src/test/java/web/service/assessmentDueDateStatusTest.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class assessmentDueDateStatusTest {
 	@Test 
 	public void feedbackExpectedWhenAssessmentOnTime(Assessment assessment) {
-		LocalDate dueDate = assessment.DateDue; 
+		LocalDate dueDate = assessment.DateDue(); 
 		Bool pastDue;
 		if(dueDate > LocalDate.now()) {
 			pastDue = true;
