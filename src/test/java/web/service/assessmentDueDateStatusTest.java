b90fe966e3ebdbcc51bb18d39bd971d5c8b93e9d
amelia <ameliaholly7@gmail.com>
2026-05-23 19:19:24 +1000

    updated test to check due date property of assessment class to see if assessment overdue

--- src/test/java/web/service/assessmentDueDateStatusTest.java
+++ src/test/java/web/service/assessmentDueDateStatusTest.java
@@ -1,9 +1,14 @@
 package web.service;
-import org.junit.Assert;
+import org.junit.*;
+import java LocalDateTime;
 
 public class assessmentDueDateStatusTest {
-	@Test
-	public void feedbackExpectedWhenAssessmentOnTime() {
-		new Assessment(); 
+	@Test 
+	public void feedbackExpectedWhenAssessmentOnTime(Assessment assessment) {
+		LocalDate dueDate = assessment.DateDue; 
+		Bool pastDue;
+		if(dueDate > LocalDate.now()) {
+			pastDue = true;
+		}
 	}
 }
