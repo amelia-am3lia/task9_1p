ff544831fa19b05d5e63930e9b21751cbb30dd9b
amelia <ameliaholly7@gmail.com>
2026-05-24 07:48:51 +1000

    second test created using first test as blue print. If the assessment is submitted after the due date, the student will not receive feedback.

--- src/test/java/web/service/web/TestTaskFeedbackService.java
+++ src/test/java/web/service/web/TestTaskFeedbackService.java
@@ -3,10 +3,20 @@ import org.junit.*;
 import java.time.LocalDate;
 
 public class TestTaskFeedbackService {
+	// if the assessment was submitted before the due date, the student will get feedback
 	@Test 
 	public void testSubmittedBeforeDueSuccess() {
 		LocalDate dueDate = LocalDate.parse("2010-10-10"); 
 		LocalDate dateSubmitted = LocalDate.parse("2010-01-01");
 		Assert.assertTrue(TaskFeedbackService.Verification(dateSubmitted, dueDate) == "Feedback will be given on your assessment.");
+	}
+	// first test passed and finished, can reuse structure etc
+	
+	// if the assessmeent was submitted after the due date, the student will not receive feedback
+	@Test 
+	public void testSubmittedAfterDueFail() {
+		LocalDate dueDate = LocalDate.parse("2010-01-01"); 
+		LocalDate dateSubmitted = LocalDate.parse("2010-01-10");
+		Assert.assertFalse(TaskFeedbackService.Verification(dateSubmitted, dueDate) == "Feedback will be given on your assessment.");
 	}
 }
