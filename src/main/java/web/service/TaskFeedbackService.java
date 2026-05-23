f94470910fb226bf6fd9583fb385f40c0acb9997
amelia <ameliaholly7@gmail.com>
2026-05-24 09:39:04 +1000

    updated service class to make test successful  when submit date is null and due date is future

--- src/main/java/web/service/TaskFeedbackService.java
+++ src/main/java/web/service/TaskFeedbackService.java
@@ -3,13 +3,19 @@ import java.time.LocalDate;
 
 public class TaskFeedbackService{	
 	public static String Verification(LocalDate DateSubmitted, LocalDate DueDate){
-		if (DateSubmitted == null && LocalDate.now().isAfter(DueDate)) {
+		if (DateSubmitted == null) {
+			if (LocalDate.now().isAfter(DueDate)) {
 			return "Feedback will not be given on your assessment.";
+			}
+			if (LocalDate.now().isBefore(DueDate)) {
+				return "Feedback will be given on your assessment if submitted by the due date.";
+			}
 		}
-		else if(DateSubmitted.isBefore(DueDate) || DateSubmitted.isEqual(DueDate)) {
+		
+		if(DateSubmitted.isBefore(DueDate) || DateSubmitted.isEqual(DueDate)) {
 			return "Feedback will be given on your assessment.";
 		}
-		else if (DateSubmitted.isAfter(DueDate)) {
+		if (DateSubmitted.isAfter(DueDate)) {
 			return "Feedback will not be given on your assessment.";
 		}
 		else return "Please contact your tutor.";
