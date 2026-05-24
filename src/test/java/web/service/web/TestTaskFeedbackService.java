75dfd68f68fe9e8125ec700a3c62bb869be52bef
amelia <ameliaholly7@gmail.com>
2026-05-24 12:09:34 +1000

    add test to check that if due date is before trimester start the correct result is given

--- src/test/java/web/service/web/TestTaskFeedbackService.java
+++ src/test/java/web/service/web/TestTaskFeedbackService.java
@@ -97,4 +97,14 @@ public class TestTaskFeedbackService {
 				"Entered dates do not fall within the trimester. "
 				+ "Try again or speak with your tutor if special circumstances apply.");
 	}
+	
+	// test to check correct result if due date is  before semester start
+	@Test
+	public void testDueDateBeforeStartSuccess() {
+		LocalDate dueDate = LocalDate.parse("2025-03-30");
+		LocalDate dateSubmitted = LocalDate.parse("2026-03-10");
+		Assert.assertTrue(TaskFeedbackService.Verification(dateSubmitted, dueDate) == 
+				"Entered dates do not fall within the trimester. "
+				+ "Try again or speak with your tutor if special circumstances apply.");
+	}
 }
