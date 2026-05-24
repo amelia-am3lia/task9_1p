493ebca4efe80fec9b27f1918be53d0e6c07a545
amelia <ameliaholly7@gmail.com>
2026-05-24 12:59:07 +1000

    add boundary test for max submit date

--- src/test/java/web/service/web/TestTaskFeedbackService.java
+++ src/test/java/web/service/web/TestTaskFeedbackService.java
@@ -218,4 +218,11 @@ public class TestTaskFeedbackService {
 		Assert.assertTrue(TaskFeedbackService.Verification(dateSubmitted, dueDate) == 
 				"Feedback will not be given on your assessment.");
 	}
+	@Test
+	public void testMaxSubmitSuccess() {
+		LocalDate dateSubmitted = LocalDate.parse("2026-08-07");
+		LocalDate dueDate = LocalDate.parse("2026-05-07");
+		Assert.assertTrue(TaskFeedbackService.Verification(dateSubmitted, dueDate) == 
+				"Feedback will be given on your assessment.");
+	}
 }
